9acb4aeff02664893868973ee74798fa5731485b
Oleg Abdrashitov <olegabu73@gmail.com>
2023-06-26 15:12:18 -0400

    mainnet only

--- destroy.sh
+++ destroy.sh
@@ -4,16 +4,13 @@ env | grep 'namespace\|db_host\|image'
 
 function fun() {
     sui_network=$1
+    sui_shard=$2
 
-    #envsubst < deploy.yaml | kubectl --namespace $namespace -f - delete
-
-    cat deploy.yaml | \
-    sed 's/${namespace}/namespace/g' | sed "s/namespace/$namespace/g" | \
-    sed 's/${sui_network}/sui_network/g' | sed "s/sui_network/$sui_network/g" | \
-    sed 's/${image_sui_archive}/image_sui_archive/g' | sed "s@image_sui_archive@${image_sui_archive}@g" |\
-    sed 's/${db_password_sui_archive}/db_password_sui_archive/g' | sed "s/db_password_sui_archive/$db_password_sui_archive/g" | \
-    sed 's/${db_host}/db_host/g' | sed "s/db_host/$db_host/g" | \
-    kubectl --namespace $namespace -f - delete
+    kubectl --namespace ${namespace} delete deployment sui-archive-${sui_network}${sui_shard}
 }
 
-fun devnet
+#fun devnet
+
+#fun testnet
+
+fun mainnet
